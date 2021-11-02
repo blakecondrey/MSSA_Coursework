ee6bc3684bde1da513cc227e2e8706e62b6ce6ea
blakecondrey <79805333+blakecondrey@users.noreply.github.com>
2021-11-01 22:10:17 -0600

    Add files via upload

---
+++ Assignments/20483C - Programming in C#/1/c_sharp/Assignment 1/Assignment 1/Assignment 1/Calculations.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    class Calculations
+    {
+        public static void Multiply2Values()
+        {
+            try {
+                Console.WriteLine("Give me any number greater than 0.");
+                float numOne = float.Parse(Console.ReadLine());
+                Console.WriteLine("Give me any number to multiply it with except for zero");
+                float numTwo = float.Parse(Console.ReadLine());
+                float result = numOne * numTwo;
+                Console.WriteLine($"{numOne} x {numTwo} = {result}", result);
+
+            }
+            catch (DivideByZeroException e) {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+        public static void Divide2Values()
+        {
+            try {
+                Console.WriteLine("Give me any number.");
+                float numOne = float.Parse(Console.ReadLine());
+                Console.WriteLine("Give me any number to divide by except for zero");
+                float numTwo = float.Parse(Console.ReadLine());
+                float result = numOne / numTwo;
+                Console.WriteLine($"{numOne} / {numnTwo} = {result}", result);
+            }
+
+            catch (DivideByZeroException e) {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+        public static void Compare2Values()
+        {
+            Console.WriteLine("Give me any number.");
+            float compare1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Give me another number.");
+            float compare2 = float.Parse(Console.ReadLine());
+
+            if (compare2 > compare1)
+            {
+                Console.WriteLine($"{compare2} is greater than {compare1}.");
+            }
+            else if (compare2 == compare1)
+            {
+                Console.WriteLine($"{compare2} is equal to {compare1}.");
+            }
+            else
+            {
+                Console.WriteLine($"{compare2} is less than {compare1}.");
+            }
+        }
+        public static void EvenOrOdd()
+        {
+            Console.WriteLine("Give me any integer.");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 1)
+            {
+                Console.WriteLine($"{number1} is an odd number");
+            }
+            else
+            {
+                Console.WriteLine($"{number1} is an even number");
+            }
+        }
+    }
+}
---
+++ Assignments/20483C - Programming in C#/1/c_sharp/Assignment 1/Assignment 1/Assignment 1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = false;
+            do
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Please input an option number (1-6), 7 to exit.");
+                Console.WriteLine("1: Calculate the income of an employee.");
+                Console.WriteLine("2: Calculate if you passed or failed a class.");
+                Console.WriteLine("3: Multiply two values.");
+                Console.WriteLine("4: Divide one value by another.");
+                Console.WriteLine("5: Compare two values and return the biggest value.");
+                Console.WriteLine("6: Determine if a number is odd or even.");
+                Console.WriteLine("7: Exit");
+
+                string optionStr = Console.ReadLine();
+                int option = int.Parse(optionStr);
+
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("You have chosen option {0}", option, "\n");
+                        Employee.EmployeeIncome();
+                        flag = true;
+                        break;
+                    case 2:
+                        Console.WriteLine("You have chosen option {0}", option, "\n");
+                        Student.PassOrFail();
+                        flag = true;
+                        break;
+                    case 3:
+                        Console.WriteLine("You have chosen option {0}", option, "\n");
+                        Calculations.Multiply2Values();
+                        flag = true;
+                        break;
+                    case 4:
+                        Console.WriteLine("You have chosen option {0}", option, "\n");
+                        Calculations.Divide2Values();
+                        flag = true;
+                        break;
+                    case 5:
+                        Console.WriteLine("You have chosen option {0}", option, "\n");
+                        Calculations.Compare2Values();
+                        flag = true;
+                        break;
+                    case 6:
+                        Console.WriteLine("You have chosen option {0}", option, "\n");
+                        Calculations.EvenOrOdd();
+                        flag = true;
+                        break;
+                    case 7:
+                        Console.WriteLine("You have chosen to exit.");
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("You did not select a valid option.");
+                        break;
+                }
+            } while (flag);
+        }
+    }
+}
